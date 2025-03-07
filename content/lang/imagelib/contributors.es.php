3c404057f637e2ea6810ff83b957320bbec4eb07
Samanta Orellana <kasaore@gmail.com>
2025-03-07 10:26:53 -0600

    Update contributors.es.php

--- content/lang/imagelib/contributors.es.php
+++ content/lang/imagelib/contributors.es.php
@@ -7,8 +7,8 @@ Language: Español (Spanish)
 
 $LANG['NAV_HOME'] = 'Inicio';
 $LANG['NAV_IMG_LIB'] = 'Biblioteca de Imágenes';
-$LANG['NAV_IMG_CONTR'] = 'Colaboradores de Imágenes';
+$LANG['NAV_IMG_CONTR'] = 'Proveedores de Imágenes';
 $LANG['SPECIMENS'] = 'Especímenes';
-$LANG['IMG_CONTR'] = 'Contribuidores de imágenes';
+$LANG['IMG_CONTR'] = 'Proveedores de imágenes';
 $LANG['OBS'] = 'Observaciones';
 ?>
